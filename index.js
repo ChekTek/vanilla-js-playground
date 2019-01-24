215c82689c55acb304872c1cecf992acd3f6f512
Erik Munoz <6412879+NYPD@users.noreply.github.com>
2019-01-24 01:02:02 -0500

    Switch js to use two spaces

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 class TaskList {
-    constructor(divElementId) {
-        this.self = document.getElementById(divElementId);
-        this.self.innerHTML += '<input class="new-task-input" value="" />';
-        this.self.innerHTML += '<button class="add-task-button">Add Task</button>';
-        this.self.innerHTML += '<div class="task-container"></div>';
-        this.taskInput = this.self.querySelector('.new-task-input');
-        this.taskContainer = this.self.querySelector('.task-container');
-        this.taskButton = this.self.querySelector('.add-task-button');
-        this.tasks = [];
-        this.taskButton.addEventListener('click',this.addTask.bind(this));
-    }
+  constructor(divElementId) {
+      this.self = document.getElementById(divElementId);
+      this.self.innerHTML += '<input class="new-task-input" value="" />';
+      this.self.innerHTML += '<button class="add-task-button">Add Task</button>';
+      this.self.innerHTML += '<div class="task-container"></div>';
+      this.taskInput = this.self.querySelector('.new-task-input');
+      this.taskContainer = this.self.querySelector('.task-container');
+      this.taskButton = this.self.querySelector('.add-task-button');
+      this.tasks = [];
+      this.taskButton.addEventListener('click',this.addTask.bind(this));
+  }
 
-    addTask() {
-        let newTask = this.taskInput.value;
-        this.tasks.push(newTask);
-        this.taskContainer.innerHTML += `<div class="task">${newTask}</div>`;
-    }
+  addTask() {
+      let newTask = this.taskInput.value;
+      this.tasks.push(newTask);
+      this.taskContainer.innerHTML += `<div class="task">${newTask}</div>`;
+  }
 }
 
 var myList = new TaskList('my-task-list');
